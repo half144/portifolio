5f5410328daee0280c1b0e8ac265384f39485389
half144 <99549268+half144@users.noreply.github.com>
2022-09-11 20:33:36 -0300

    Update Initial.jsx

--- src/src/pages/Initial/Initial.jsx
+++ src/src/pages/Initial/Initial.jsx
@@ -18,7 +18,7 @@ const Initial = () => {
                 <h1>Rafael</h1>
                 <h3>web dev</h3>
                 <h3>ui designer</h3>
-               /* <button className="btn" id="contactbutton" onClick={() => navigate("/contact")}>contact</button> */
+               
             </div>
             <div className="img" data-aos="zoom-in" data-aos-delay="400" data-aos-duration="1500">
                 <img id="astro" src={astronaut} alt="" />
