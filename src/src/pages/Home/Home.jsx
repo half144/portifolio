e390e791a13fe019d8ca131f4b882a850c26adff
half144 <99549268+half144@users.noreply.github.com>
2022-09-11 13:50:11 -0300

    Update Home.jsx

--- src/src/pages/Home/Home.jsx
+++ src/src/pages/Home/Home.jsx
@@ -2,14 +2,16 @@ import About from "../About/About"
 import Initial from "../Initial/Initial"
 import Projects from "../Projects/Projects"
 import Stacks from "../Stacks/Stacks"
+import Contact from "../Contact/Contact"
 
 const Home = () => {
     return (
         <>
             <Initial />
             <About />
-            <Projects />
             <Stacks />
+            <Projects />
+            <Contact />
         </>
     )
 }
