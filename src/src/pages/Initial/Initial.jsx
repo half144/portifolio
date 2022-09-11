669662396735a85e2d4de5b46d1d9a6f547277eb
half144 <99549268+half144@users.noreply.github.com>
2022-09-11 13:58:09 -0300

    Update Initial.jsx

--- src/src/pages/Initial/Initial.jsx
+++ src/src/pages/Initial/Initial.jsx
@@ -12,7 +12,7 @@ const Initial = () => {
     }, [])
 
     return (
-        <div className="home">
+        <div className="home" data-aos="fade-left">
             <div className="whoiam" data-aos="fade-up">
                 <p>oi, sou o</p>
                 <h1>Rafael</h1>
