191e80a1c5c36dc00c3b02124716b5e50f4def1b
half144 <99549268+half144@users.noreply.github.com>
2022-10-12 18:10:40 -0300

    Update About.jsx

--- src/src/pages/About/About.jsx
+++ src/src/pages/About/About.jsx
@@ -15,9 +15,7 @@ const About = () => {
             </div>
             <div className="aboutText" data-aos="fade-up">
                 <h1 >Sobre</h1>
-                <p>Me chamo Rafael, sou um desenvolvedor Front End. Dei início em minha caminhada de UI Design no começo de 2022. Começei os estudos em desenvolvimento web, e venho aprimorando meus conhecimentos e técnicas.
-                    Recentemente, estreei a ponderação em a Biblioteca React.JS. Pretendo entrar na faculdade em 2023, e cursar "Análise e desenvolvimento de Sistemas".
-                    Desenvolvo interfaces modernas e de alta qualidade, buscando performace, modernidade e responsividade.</p>
+                <p>Me chamo Rafael, tenho 17 anos e sempre fui apaixonado por tecnologia, meu primeiro contato com um computador foi em 2007, quando eu tinha 3 anos, mas foi com 9 que me deparei pela primeira vez com desenvolvimento web, quando eu decidi criar um fansite de Habbo Hotel para postar dicas de como pegar emblemas no jogo, com 15 tive meu primeiro contato com JavaScript, mas apenas por diversão, eu ainda não sabia o que queria ser, com 16 me decidi, e desde então, dedico todo meu tempo livre para estudar, ate meu tempo não livre pois se tornou um vicio, me apaixonei de tal forma que se tornou uma obsessão, saio de casa pensando na hora de voltar para poder terminar meus projetos pessoais ou aprender algo novo que ainda não sei, é realmente o que eu quero pra vida e o que eu sinto que nasci pra ser.</p>
             </div>
         </div>
     )
